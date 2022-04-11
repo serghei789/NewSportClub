59bb44104b5720eccfdbbd0faa5066ebf8604c35
Serghei <78849747+serghei789@users.noreply.github.com>
2022-04-11 14:14:33 +0300

    added vent about modal with id

--- client/src/components/common/MoreEvent/MoreEvent.jsx
+++ client/src/components/common/MoreEvent/MoreEvent.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 
-function MoreEvent() {
+function MoreEvent({id}) {
   
   return (
     <div>
+      {id}
       <p><strong>Волейбол</strong></p>
       <p>подробно о событии</p>
     </div>
