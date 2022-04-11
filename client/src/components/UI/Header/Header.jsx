cfc1546860c88bb20a381c6df517abe434895e10
Serghei <78849747+serghei789@users.noreply.github.com>
2022-04-11 19:28:51 +0300

    put header modals

--- client/src/components/UI/Header/Header.jsx
+++ client/src/components/UI/Header/Header.jsx
@@ -2,13 +2,13 @@ import './Header.scss'
 import { useState } from 'react'
 import AddEventForm from '../../common/AddEventForm/AddEventForm'
 import { Button, Modal } from 'react-bootstrap'
+import AddEventModal from '../../modals/AddEventModal/AddEventModal'
+import LoginModal from '../../modals/LoginModal/LoginModal'
+import RegistrationModal from '../../modals/RegistrationModal/RegistrationModal'
 
 const Header = () => {
   
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  
 
   return (
     <header className='header container'>
@@ -16,29 +16,14 @@ const Header = () => {
           <AddEventForm />
         </Modal> */}
       <div className="header__left">
-        <Button variant="primary" onClick={handleShow}>
-          <img src="/assets/Group1plus.png" alt="" className="header__item header__item--plus"/>
-        </Button>
+        <AddEventModal />
       </div>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
+      
       <div className="header__right">
         <div className="header__items">
-          <button className="header__item header__button">Signup</button>
-          <button className="header__item header__button">Login</button>
+          <RegistrationModal />
+          <LoginModal />
+          <button className="header__item header__button">Logout</button>
           <img src="/assets/account.png" alt={''} className="header__item header__item--account" />
         </div>
       </div>
---
+++ client/src/components/modals/LoginModal/LoginModal.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+export default function LoginModal() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+  return(
+  <>
+        <button onClick={handleShow} className="header__item header__button">Login</button>
+        {/* <div onClick={handleShow}>
+          <img src="/assets/Group1plus.png" alt="" className="header__item header__item--plus"/>
+        </div> */}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Авторизация
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+  </>
+  )
+}
